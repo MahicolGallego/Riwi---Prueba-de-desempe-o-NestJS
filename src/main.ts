9d4788ec698968fee6c2af7ea72ace841ba88242
Mahi_G13 <maicolgallego14@gmail.com>
2024-10-07 17:24:16 -0500

    chore(general): Active validation pipe, cors, serialization, and swagger to documentation;feat(error-handling): Add centralized error management;

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  // use pipe for data validation of request and data transform
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  //enable cors to resolve conflicts with Cross-Origin
+  app.enableCors();
+
+  //enable serializacion to active class-transformer funtionality(data control for response)
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
 
   //Swagger for documentation of API
   const config = new DocumentBuilder()
@@ -20,5 +38,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/library/api/v1/docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
