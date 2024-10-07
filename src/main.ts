51bea312f900c9b0d89ec9cefd74f3e19089bbf9
Mahi_G13 <maicolgallego14@gmail.com>
2024-10-07 17:15:40 -0500

    wip: define database models and set up TypeORM connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  //Swagger for documentation of API
+  const config = new DocumentBuilder()
+    .setTitle('eSports Arena')
+    .setDescription('')
+    .setVersion('1.0')
+    // add API key as authentication method
+    .addApiKey({
+      name: 'x-api-key',
+      type: 'apiKey',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/library/api/v1/docs', app, document);
 }
 bootstrap();
---
+++ src/matches/entities/match.entity.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { Result } from 'src/results/entities/result.entity';
+import { Tournament } from 'src/tournaments/entities/tournament.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('matches')
+export class Match {
+  // Define properties of format responses with users for swagger documentation with @ApiProperty decorator
+  @ApiProperty()
+  // Expose properties for serialization. It will be included in the response when converting the entity to JSON.
+  @Expose()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty()
+  @Expose()
+  @Column({ type: 'uuid', nullable: false })
+  tournament_id: string;
+
+  @ApiProperty()
+  @Expose()
+  @Column({ type: 'uuid', nullable: false })
+  player_1_id: string;
+
+  @ApiProperty()
+  @Expose()
+  @Column({ type: 'uuid', nullable: false })
+  player_2_id: string;
+
+  // Exclude properties from serialization. It will not be included in the response when converting the entity to JSON.
+  @Exclude()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => Tournament, (tournament) => tournament.matches)
+  @JoinColumn({ name: 'tournament_id' })
+  tournament: Tournament;
+
+  @ManyToOne(() => User, (user) => user.matchesAsPlayer1)
+  @JoinColumn({ name: 'player_1_id' })
+  player1: User;
+
+  @ManyToOne(() => User, (user) => user.matchesAsPlayer2)
+  @JoinColumn({ name: 'player_2_id' })
+  player2: User;
+
+  @OneToOne(() => Result, (result) => result.match)
+  results: Result;
+}
